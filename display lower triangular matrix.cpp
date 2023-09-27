e596bf48703a949957e5e76a8ef13f6bb88fc7f7
poojanighut0307 <142563622+poojanighut0307@users.noreply.github.com>
2023-09-27 17:01:04 +0530

    Add files via upload

---
+++ display lower triangular matrix.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],r,c,i,j;
+	printf("enter number of row and colums = ");
+	scanf("%d%d",&r,&c);
+	printf("\n enter matrix = ");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("display matrix =\n ");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			if(i>j)
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+
+}
