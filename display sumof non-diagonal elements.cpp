f4af8ebe6b0f4ea9d0e73ef98dfcbb73d527fc60
poojanighut0307 <142563622+poojanighut0307@users.noreply.github.com>
2023-10-02 19:38:49 +0530

    Add files via upload

---
+++ display sumof non-diagonal elements.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],r,c,i,j,s=0;
+	printf("enter number of row and colums = ");
+	scanf("%d%d",&r,&c);
+	printf("\n enter matrix = ");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			if(i!=j)
+			s=s+a[i][j];
+		}
+	}
+	printf("sum of non-diagonal elements %d",s);
+}
