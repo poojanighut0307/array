82ca5521e951e8fdda3774758d8050905e86490e
poojanighut0307 <142563622+poojanighut0307@users.noreply.github.com>
2023-10-06 21:14:17 +0530

    Add files via upload

---
+++ TRANSPOSE OF MATRIX.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],r,c,i,j;
+	printf("enter number of row and colums = ");
+	scanf("%d%d",&r,&c);
+	printf("\n enter matrix = ");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("\nTRANSPOSE matrix =\n ");
+	for(i=0;i<c;i++)
+	{
+		for(j=0;j<r;j++)
+		{
+			printf("%d\t",a[j][i]);
+		}
+		printf("\n");
+	}
+}
